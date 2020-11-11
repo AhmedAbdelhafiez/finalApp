8c60e776d08fa1af02aeaa9b74b64fa0170d5e9f
AhmedAbdelhafiez <Ahmed.Abdelhafiez@trianglz.com>
2020-11-11 15:17:20 +0200

    test

---
+++ test/controllers/password_resets_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get password_resets_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get password_resets_update_url
+    assert_response :success
+  end
+
+end
